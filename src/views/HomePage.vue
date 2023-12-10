a0e369ac29301c3ddabc7a46d286e29a9e4be6a9
Udomsark Amnuaysookwong <udomsark1863@hotmail.com>
2023-12-10 16:33:46 +0700

    update learn Vue3 2023-12-10

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,8 @@
 <template>
-  <v-row>
-    <v-col>test</v-col>
+  <v-row class="h-100vh">
+    <v-col cols="12">
+      <v-card class="h-100 d-flex align-center justify-center" variant="tonal">TestVueJS</v-card>
+    </v-col>
   </v-row>
 </template>
 
