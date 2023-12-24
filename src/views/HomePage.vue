8ccfcd72797ad453efb101e2c9641634d88c1da5
Udomsark Amnuaysookwong <udomsark1863@hotmail.com>
2023-12-24 14:52:38 +0700

    update 2023-12-24

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,7 @@
   <v-row class="h-100vh">
     <v-col cols="12">
       <v-card class="h-100 d-flex align-center justify-center" variant="tonal">
-        <router-link to="/"><v-btn>ไปหน้า /Demo1 (กำหนด router)</v-btn></router-link>
+        <router-link to="/Demo1"><v-btn>ไปหน้า /Demo1 (กำหนด router)</v-btn></router-link>
       </v-card>
     </v-col>
   </v-row>
